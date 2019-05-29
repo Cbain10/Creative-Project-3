847784c3ff1babb2e7f2d564c6361a01ba15ade7
Cbain10 <50216002+Cbain10@users.noreply.github.com>
2019-05-29 16:14:05 -0600

    video embedded

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+Vue.component('star-rating', VueStarRating.default);
+
 let app = new Vue ({
     el: '#app',
     data: {
@@ -5,21 +7,31 @@ let app = new Vue ({
             quote: '',
             author: '',
             season: '',
-            episode: '',
-            image: ''
-        }
+            episode: ''
+        },
+        ratings: {
+            total: 0,
+            sum: 0,
+            average: 0
+        },
+        loading: true
     },
     methods: {
         async seinfeld() {
             try {
+                this.loading = true;
                 const response = await axios.get('https://seinfeld-quotes.herokuapp.com/random');
                 this.current = response.data;
+                this.loading = false;
                 console.log(response);
             } catch (error) {
                 console.log(error);
             }
+        },
+        setRating(rating) {
+            this.ratings[this.current].total += 1;
+            this.ratings[this.current].sum += rating;
+            this.ratings[this.current].average = this.ratings[this.current].sum / this.ratings[this.current].total;
         }
-
-    }
-
+    },
 });
