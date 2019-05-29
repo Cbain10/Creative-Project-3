9fb4182599e4f330e37f84618bd0a0ad42143f16
Cbain10 <50216002+Cbain10@users.noreply.github.com>
2019-05-29 16:37:33 -0600

    nav bar initial commit

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ Vue.component('star-rating', VueStarRating.default);
 let app = new Vue ({
     el: '#app',
     data: {
+        number: 1,
         current: {
             quote: '',
             author: '',
@@ -13,6 +14,10 @@ let app = new Vue ({
             total: 0,
             sum: 0,
             average: 0
+        },
+        addedName: '',
+        addedComment: '',
+        comments: {
         },
         loading: true
     },
@@ -28,10 +33,24 @@ let app = new Vue ({
                 console.log(error);
             }
         },
-        setRating(rating) {
-            this.ratings[this.current].total += 1;
-            this.ratings[this.current].sum += rating;
-            this.ratings[this.current].average = this.ratings[this.current].sum / this.ratings[this.current].total;
+        // setRating(rating) {
+        //     this.ratings[this.current].total += 1;
+        //     this.ratings[this.current].sum += rating;
+        //     this.ratings[this.current].average = this.ratings[this.current].sum / this.ratings[this.current].total;
+        // },
+        addComment() {
+            if (!(this.number in this.comments))
+                Vue.set(app.comments, this.number, new Array);
+            this.comments.push({
+                author: this.addedName,
+                text: this.addedComment,
+                time: moment().format('LLLL'),
+            });
+            this.addedName = '';
+            this.addedComment = '';
+            // this.comments.name = this.addedName;
+            // this.comments.words = this.addedComment;
+            // this.comments.time = moment().format('LLLL');
         }
     },
 });
